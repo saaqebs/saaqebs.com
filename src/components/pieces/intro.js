ec4d2516de7d80d24ed0d944fda40c3f4b3482d0
Saaqeb <ss3759@cornell.edu>
2020-01-08 14:52:31 -0500

    layout for entire website fixed

--- src/components/pieces/intro.js
+++ src/components/pieces/intro.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 class Intro extends Component {
   render() {
     return (
-      <div className="container vertical-center-parent">
+      <div className="container">
         <div className="intro">
+
           <h1> Saaqeb Siddiqi </h1>
           <h2 className="noselect"> Developer. Designer. Teacher. </h2>
           <div className="info-container">
+
             <div className="info-second-container">
               <p className="noselect">
                 Hello, my name is Saaqeb! I am a junior at
@@ -15,12 +17,14 @@ class Intro extends Component {
                 designing and analyzing all sorts of software applications.
               </p>
               <p> Take a look at some of my projects below! </p>
-              <div className="button-position">
+
+              <div className="button-position ">
                 <section id="section03" className="demo">
                   <a href="#portfolio"><span />Scroll</a>
                 </section>
               </div>
             </div>
+
           </div>
         </div>
       </div>
