11388b406add5b35ed0250eeab807c33755a6fc9
Saaqeb <ss3759@cornell.edu>
2020-01-09 01:14:29 -0500

    (hopefully) fixing ios issue

--- src/components/content/homepage.js
+++ src/components/content/homepage.js
@@ -4,9 +4,9 @@ import React, { Component } from 'react';
 class Homepage extends Component {
   render() {
     return (
-      <section className="homepage">
+      <div className="homepage">
         {/* Get Rekt */}
-      </section>
+      </div>
     );
   }
 }
