328d9bb6ecc60f3b9f2d1f21ebc9ac49800948ce
Saaqeb <ss3759@cornell.edu>
2021-09-06 23:19:42 -0400

    picture bug

--- src/pages/home/projects/data/descriptions.js
+++ src/pages/home/projects/data/descriptions.js
@@ -3,7 +3,7 @@ const projectList = [
     id: 0,
     name: "Tracking & Exploring COVID-19 Data in New Jersey",
     description: "A repository containing the ongoing data collection of COVID-19 cases per each municipality in New Jersey. Also included elementary forecasting and analysis!",
-    image: require("../images/covid.jpg"),
+    image: require("../images/covid.jpg").default,
     buttons: [
       {
         id: 0,
@@ -21,7 +21,7 @@ const projectList = [
     id: 1,
     name: "Safer Google Search",
     description: "A safer way to Google search that does not send invasive ad information about your device when searching. Protect yourself while browsing the web!",
-    image: require("../images/safergoogle.png"),
+    image: require("../images/safergoogle.png").default,
     buttons: [
       {
         id: 0,
@@ -39,7 +39,7 @@ const projectList = [
     id: 2,
     name: "Chess",
     description: "A project my team and I built for CS 3110 that includes an easy and hard AI to play against. Built using OCaml, the best programming language in the world!",
-    image: require("../images/chess.jpeg"),
+    image: require("../images/chess.jpeg").default,
     buttons: [
       {
         id: 0,
@@ -52,7 +52,7 @@ const projectList = [
     id: 3,
     name: "Tastes Like Home",
     description: "An app that uses ad hoc information retrieval using the jaccard similarity to find a new restaurant in a specific city that matches your favorite restaurant!",
-    image: require("../images/tasteslikehome.png"),
+    image: require("../images/tasteslikehome.png").default,
     buttons: [
       {
         id: 0,
